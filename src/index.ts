000ff309c28cb13ba9543f44754fe73309095350
a2yomi <a2yomidiscord@gmail.com>
2025-11-27 23:35:25 +0300

    chore: use only html

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,75 @@
-import * as html from "./html.js";
+function link<D extends string, T extends string>(display: D, url: T): string {
+  return `<a href="${url}">${display}</a>`;
+}
 
-export const fmt = {
-  html,
-};
+function mention<T extends string | number>(usr: T) {
+  if (typeof usr === "string") {
+    return `t.me/${usr}`;
+  }
+  return `tg://user?id=${usr}`;
+}
+
+function bold<T extends string>(text: T): `<b>${T}</b>` {
+  return `<b>${withEscape(text)}</b>`;
+}
+
+function cursive<T extends string>(text: T): `<i>${T}</i>` {
+  return `<i>${withEscape(text)}</i>`;
+}
+
+function code<T extends string>(text: T): `<code>${T}</code>`;
+
+function code<T extends string, L extends string>(
+  text: T,
+  lang: L
+): `<pre language="${L}">${T}</pre>`;
+
+function code<T extends string, L extends string | undefined = undefined>(
+  text: T,
+  lang?: L
+) {
+  if (lang) {
+    return `<pre language="${lang}">${withEscape(text)}</pre>`;
+  }
+
+  return `<code>${withEscape(text)}</code>`;
+}
+
+function strike<T extends string>(text: T): `<s>${T}</s>` {
+  return `<s>${withEscape(text)}</s>`;
+}
+
+function underline<T extends string>(text: T): `<u>${T}</u>` {
+  return `<u>${withEscape(text)}</u>`;
+}
+
+function withEscape<T extends string>(s: T): T {
+  const allowedTags = [
+    "b",
+    "strong",
+    "u",
+    "s",
+    "del",
+    "strike",
+    "pre",
+    "code",
+    "em",
+    "i",
+  ];
+
+  const tagPattern = /<(\/?)([a-zA-Z][^>]*)>/g;
+
+  const result = s.replaceAll(tagPattern, (match, _, tagName) => {
+    const cleanTagName = tagName.split(/\s/)[0].toLowerCase();
+
+    if (allowedTags.includes(cleanTagName)) {
+      return match;
+    } else {
+      return match.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    }
+  });
+
+  return result as T;
+}
+
+export { bold, code, cursive, link, mention, strike, underline };
